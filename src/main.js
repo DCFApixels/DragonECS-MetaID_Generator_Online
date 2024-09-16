1fa395c312497ef89f006931b2311d782d8179b8
Mikhail <99481254+DCFApixels@users.noreply.github.com>
2024-09-16 14:53:59 +0800

    add copy button

--- src/main.js
+++ src/main.js
@@ -1,8 +1,17 @@
-
+async function CopyToClipboard(text) {
+    try {
+        await navigator.clipboard.writeText(text);
+        console.log('Идентификатор скопирован в буфер обмена');
+    } catch (err) {
+        console.error('Не удалось скопировать идентификатор в буфер обмена: ', err);
+    }
+}
 
 
 var metaIDField = document.getElementById("meta_id_field");
+var metaIDCopyButton = document.getElementById("meta_id_copy_button");
 var metaIDAttributeField = document.getElementById("meta_id_attribute_field");
+var metaIDAttributeCopyButton = document.getElementById("meta_id_attribute_copy_button");
 var button = document.getElementById("generate_button");
 
 
@@ -12,5 +21,12 @@ function OnGenerateButtonClick(target){
     metaIDField.value = id;
     metaIDAttributeField.value = '[MetaID("' + id + '")]';
 }
+function Copy(target)
+{
+    let elem = this;
+    CopyToClipboard(elem.value);
+}
 
 button.addEventListener("click", OnGenerateButtonClick);
+metaIDCopyButton.addEventListener("click", Copy.bind(metaIDField));
+metaIDAttributeCopyButton.addEventListener("click", Copy.bind(metaIDAttributeField));
